1c71091d4668a7f46c11513993fe608c669e5416
Jaival <saijajaival@gmail.com>
2021-07-02 21:02:57 +0530

    script added

---
+++ config.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt-get install curl -y
+
+sudo apt-get install git -y
+
+sudo apt-get install zsh -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+git clone https://github.com/powerline/fonts.git --depth=1
+
+cd fonts
+
+./install.sh
+
+cd ..
+
+rm -rf fonts
+
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+git clone https://github.com/b4b4r07/enhancd.git
+
+zplug "b4b4r07/enhancd", use:init.sh
+
+sudo apt-get install fzy
